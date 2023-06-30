f1adf6ace12b2a5783b1ebaca2ee3230646b57a4
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2023-06-30 11:22:11 +0100

    refactor: unit and integration tests

--- src/test/java/com/parthibanrajasekaran/ProductControllerTest.java
+++ src/test/java/com/parthibanrajasekaran/ProductControllerTest.java
@@ -1,33 +1,37 @@
-// ProductControllerTest.java
-package com.parthibanrajasekaran.controller;
+package com.parthibanrajasekaran;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parthibanrajasekaran.controller.ProductController;
 import com.parthibanrajasekaran.model.Product;
 import com.parthibanrajasekaran.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ProductController.class)
+@ExtendWith(MockitoExtension.class)
 public class ProductControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
 
     @MockBean
     private ProductService productService;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     private ObjectMapper objectMapper;
 
@@ -37,16 +41,27 @@ public class ProductControllerTest {
     }
 
     @Test
+    public void testGetAllProducts() throws Exception {
+        Product product1 = new Product("1", "Product1");
+        Product product2 = new Product("2", "Product2");
+        List<Product> products = Arrays.asList(product1, product2);
+
+        when(productService.getAllProducts()).thenReturn(products);
+
+        mockMvc.perform(get("/products")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
     public void testCreateProduct() throws Exception {
         Product product = new Product("1", "Product1");
         when(productService.createProduct(product)).thenReturn(product);
 
         mockMvc.perform(post("/products")
-                        .content(objectMapper.writeValueAsString(product))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value("1"))
-                .andExpect(jsonPath("$.name").value("Product1"));
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(product)))
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -56,8 +71,6 @@ public class ProductControllerTest {
 
         mockMvc.perform(get("/products/1")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value("1"))
-                .andExpect(jsonPath("$.name").value("Product1"));
+                .andExpect(status().isOk());
     }
 }
--- src/test/java/com/parthibanrajasekaran/ProductServiceTest.java
+++ src/test/java/com/parthibanrajasekaran/ProductServiceTest.java
@@ -1,42 +1,70 @@
-// ProductServiceTest.java
-package com.parthibanrajasekaran.service;
+package com.parthibanrajasekaran;
 
 import com.parthibanrajasekaran.model.Product;
 import com.parthibanrajasekaran.repository.ProductRepository;
+import com.parthibanrajasekaran.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
-    private ProductService productService;
+    @Mock
     private ProductRepository productRepository;
+
+    private ProductService productService;
 
     @BeforeEach
     public void setup() {
-        productRepository = Mockito.mock(ProductRepository.class);
         productService = new ProductService(productRepository);
     }
 
     @Test
-    public void testCreateProduct() {
-        Product product = new Product("1", "Product1");
-        Mockito.when(productRepository.save(product)).thenReturn(product);
+    public void testGetAllProducts() {
+        Product product1 = new Product("1", "Product1");
+        Product product2 = new Product("2", "Product2");
+        List<Product> products = Arrays.asList(product1, product2);
 
-        Product createdProduct = productService.createProduct(product);
-        assertEquals(product, createdProduct);
+        when(productRepository.findAll()).thenReturn(products);
+
+        List<Product> result = productService.getAllProducts();
+        assertEquals(result, products);
     }
 
     @Test
     public void testGetProductById() {
         Product product = new Product("1", "Product1");
-        Mockito.when(productRepository.findById("1")).thenReturn(Optional.of(product));
+        when(productRepository.findById("1")).thenReturn(Optional.of(product));
 
-        Optional<Product> retrievedProduct = productService.getProductById("1");
-        assertEquals(product, retrievedProduct.orElse(null));
+        Optional<Product> result = productService.getProductById("1");
+        assertEquals(result.get(), product);
+    }
+
+    @Test
+    public void testCreateProduct() {
+        Product product = new Product("1", "Product1");
+        when(productRepository.save(product)).thenReturn(product);
+
+        Product result = productService.createProduct(product);
+        assertEquals(result, product);
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Product product = new Product("1", "Product1");
+        doNothing().when(productRepository).delete(product);
+
+        productService.deleteProduct(product);
+        verify(productRepository, times(1)).delete(product);
     }
 }
